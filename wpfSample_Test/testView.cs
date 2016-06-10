8c958564f8eae68b29d09dfc2cca0b392aa5014f
Gaurav Verma <gaurav.verma7@gmail.com>
2016-06-11 01:55:15 +0530

    Update code review changes- 10-June-2016

---
+++ wpfSample_Test/testView.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WPFSample;
+using WPFSample.Repository;
+using WPFSample.User;
+using WPFSample.User.ViewModel;
+namespace wpfSample_Test
+{
+    [TestClass]
+    public class testView
+    {
+        userViewModel objViewModel = new userViewModel();    
+       [TestMethod]
+        public void testRep()
+        {
+            WebApi owebApiObj = new WebApi();
+            owebApiObj.getAllUsers();
+        }
+       [TestMethod]
+       public void testViewModel()
+       {                 
+           objViewModel.allUsers();
+       }
+       [TestMethod]
+       public void testComboUserId()
+       {
+           objViewModel.comboUserID();
+       }
+       [TestMethod]
+       public void testComboUserId()
+       {
+           objViewModel.comboUserID();
+       }
+
+
+    }
+}
